f973a450849e12d82dbca5ed6c84bbed098dbbef
Nasirucode <akingbolahan12@gmail.com>
2025-06-30 11:30:22 +0100

    Fix: updated webhook log

--- xero_erpnext_integration/xero_erpnext_integration/apis/sales_invoice.py
+++ xero_erpnext_integration/xero_erpnext_integration/apis/sales_invoice.py
@@ -22,7 +22,7 @@ def get_specific_invoices():
     #   /invoices?Statuses=AUTHORISED,PAID&ContactIDs=3138017f-8ddc-420e-a159-e7e1cf9e643d,4b2df4a1-7aa5-4ce3-9e9c-3c55794c5283
     try:
         client = get_xero_client()
-        response = client.make_request("GET", f"/invoices?Statuses=PAID&IDs={invoice_names}&summaryOnly=True")
+        response = client.make_request("GET", f"/invoices?IDs={invoice_names}&Statuses=PAID&summaryOnly=True")
         
         return {
             "status": "success",
