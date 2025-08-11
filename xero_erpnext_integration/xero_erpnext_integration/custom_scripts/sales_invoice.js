ecb98803a6916953dd4d208042082d78645086c8
Nasirucode <akingbolahan12@gmail.com>
2025-08-11 09:09:47 +0100

    Fix(Sales Invoice): Added sync to xero to sales invoice

--- xero_erpnext_integration/xero_erpnext_integration/custom_scripts/sales_invoice.js
+++ xero_erpnext_integration/xero_erpnext_integration/custom_scripts/sales_invoice.js
@@ -19,6 +19,17 @@ frappe.ui.form.on('Sales Invoice', {
                 }
             });
         }
+        if(!frm.doc.custom_xero_invoice_number && frm.doc.docstatus == 1){
+            frm.add_custom_button(__('Sync Invoice in Xero'), function () {
+                frappe.call({
+                    method: 'xero_erpnext_integration.xero_erpnext_integration.custom_scripts.sales_invoice.on_submit',
+                    args: {
+                        doc: frm.doc,
+                        sync_to_xero: true
+                    }
+                });
+            });
+        }
     },
 
     customer(frm) {
